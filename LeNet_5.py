335f4b678412152076cf69787fe25ddb736d7c71
zougaoming <33510701+zougaoming@users.noreply.github.com>
2019-03-12 04:13:34 +0800

    Add files via upload

--- LeNet_5.py
+++ LeNet_5.py
@@ -4,8 +4,9 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 import zouflow as zf
-#from data.mnist.mnist import MNIST_DATA
+from data.mnist.mnist import MNIST_DATA
 from sklearn.utils import shuffle
+import dill
 
 def printImage(image):
     for i in range(28):
@@ -29,9 +30,18 @@ if __name__ == '__main__':
 
     mnist = input_data.read_data_sets("data/mnist/", reshape=False)
     x_train, y_train = mnist.train.images, mnist.train.labels
+    #x_train,y_train,_ = MNIST_DATA("data/mnist/").get_train_data(0,60000)
+    #x_train = x_train.astype(np.float64)
+    #y_train.astype(np.float64)
     x_train = np.pad(x_train, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+    #x_train = np.pad(x_train, ((0, 0), (2, 2), (2, 2)), 'constant')
+
+    #gfile.Exists(work_directory):
     param = zf.Param('./gojs/LeNet-5.txt')
     network = zf.Network(param=param)
+    save = zf.Save('./save_d.pkl', param)
+    save.load()
+    #dill.load_session('./save_d.pkl', main=network)
     # w1_init = w1_init.reshape(1,6,5,5)
     # network.param.w_1 = w1_init
     BATCH_SIZE = 128
@@ -40,29 +50,30 @@ if __name__ == '__main__':
 
 
 
-    param.Loss = zf.SquareLoss()
-    param.rate = 0.05
+    param.Loss = zf.MSELoss()
+    param.rate = 0.1
     param.Optimizer = zf.SGDandMomentumOptimizer(param)
     for e in range(100):
+        print('--------------',e)
         x_train, y_train = shuffle(x_train, y_train)
         #for offset in range(1):
         for offset in range(0, len(x_train), BATCH_SIZE):
-            #print(e,'-------------',offset)
+            print(e,'-------------',offset)
             end = offset + BATCH_SIZE
             batch_x, batch_y = x_train[offset:end], y_train[offset:end]
             # y_train = tf.one_hot(y_train, 10)
-            batch_x = batch_x.reshape(BATCH_SIZE, 1, 32, 32)
-            one_hot_y = dense_to_one_hot(batch_y, 10)
+            batch_x = batch_x.reshape(-1, 1, 32, 32)
+            batch_y = dense_to_one_hot(batch_y, 10)
 
 
             network.forward(batch_x)
 
 
-            print('out->',network.Output[:,0:2],one_hot_y[0:2])
+            print('out->',network.Output[:,0:2],batch_y[0:2])
 
-            network.setOutputDelta(one_hot_y.T)
+            network.setOutputDelta(batch_y.T)
             network.backward()
-        print(e)
+            save.store()
 
 
 
